fecd277cd6b7c0595cb9ceb1185b16f322c50469
Morgan Farrow <morgancfarrow@gmail.com>
2020-04-19 14:12:00 +0000

    Done.

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,6 +21,13 @@ def bonus
   }
 
   #code your solution here:
+epic_tragedy[:montague][:hero][:status] = "dead"
+ 
+puts epic_tragedy
+
+ epic_tragedy[:capulet][:heroine][:status] = "dead"
+
+puts epic_tragedy
 
   
 
