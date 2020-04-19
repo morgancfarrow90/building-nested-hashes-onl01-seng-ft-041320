9e6c3090fc0ea56d20c918aac7916de99fb523ef
Morgan Farrow <morgancfarrow@gmail.com>
2020-04-19 14:11:45 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -22,11 +22,11 @@ def bonus
 
   #code your solution here:
 epic_tragedy[:montague][:hero][:status] = "dead"
-epic_tragedy[:capulet][:heroine][:status] = "dead"
  
 puts epic_tragedy
 
- 
+ epic_tragedy[:capulet][:heroine][:status] = "dead"
+
 puts epic_tragedy
 
   
